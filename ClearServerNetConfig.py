01ef53698ec3774b973fdeb56b6acb833afc704d
root <christyler@126.com>
2016-08-09 15:40:01 -0700

    new file:   ClearServerNetConfig.py 	new file:   Libs/InstallNote.txt 	new file:   Libs/paramiko-2.0.2.tar.gz 	new file:   Libs/pycrypto-2.6.1.tar.gz 	new file:   SshRemote.py 	new file:   SshRemote.pyc 	new file:   __init__.py 	.idea/SetupEnvKifer.iml 	.idea/misc.xml 	.idea/modules.xml 	.idea/workspace.xml

---
+++ ClearServerNetConfig.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python
+
+import SshRemote
+import sys
+import re
+
+def clearNetworkConfig(serverIp, username='root', passwd='fortinet', serverPort=22):
+    mySsh = SshRemote.SshAgent(serverIp, username, passwd, serverPort)
+    mySsh.connectRemote()
+
+    # Delete all vlan interfaces
+    print 'Starting to delete all the VLAN interfaces on server "'+str(serverIp)+'" ...'
+    intList = mySsh.execCommand('ip link|grep -E -v "^ "|awk \'{print $2}\'|awk -F ":" \'{print $1}\'')
+    vlanIntPattern = re.compile(r'.*\..*@.*')
+    for i in intList[0]:
+        matchResult = vlanIntPattern.match(i.replace('\n', ''))
+        if matchResult:
+            mySsh.execCommand('ip link del '+i.split('@')[0])
+    print 'All the VLAN interfaces on server "'+str(serverIp)+'" have been deleted.'
+
+    # Flush all the IP addresses of interfaces
+    print 'Starting to flush all the IP addresses on server "'+str(serverIp)+'" ...'
+    intList = mySsh.execCommand('ip link|grep -E -v "^ "|awk \'{print $2}\'|awk -F ":" \'{print $1}\'')
+
+    for j in intList[0]:
+        findMgmtResult = mySsh.execCommand('ip add show '+str(j).replace('\n', '')+'|grep '+str(serverIp)+' > /dev/null 2>/dev/null; echo $?')
+        if findMgmtResult[0][0].find('0') != -1:
+            print '"'+str(j).replace('\n', '')+'" is the management interface, skip this step.'
+            continue
+        mySsh.execCommand('ip add flush dev '+j.replace('\n', ''))
+    print 'Flushing IP addresses on server "'+str(serverIp)+'" over.'
+
+
+    # Clear all the iptables rules
+    print 'Starting to clear iptables rules on server "'+str(serverIp)+'" ...'
+    mySsh.execCommand('iptables -F')
+    mySsh.execCommand('iptables -t nat -F')
+    mySsh.execCommand('iptables -t raw -F')
+    mySsh.execCommand('iptables -t mangle -F')
+    print 'Clearing iptables rules on server "'+str(serverIp)+'" over.'
+
+
+    # Clear tc rules created by easy-tc.sh
+    # tc qdisc del dev eth1 root handle 1111: prio >/dev/null 2>/dev/null
+    print 'Starting to clear the tc rules on server "'+str(serverIp)+'" created by easy-tc.sh.'
+    intList = mySsh.execCommand('ip link|grep -E -v "^ "|awk \'{print $2}\'|awk -F ":" \'{print $1}\'')
+    for k in intList[0]:
+        mySsh.execCommand('tc qdisc del dev '+str(k).replace('\n', '')+' root handle 1111: prio >/dev/null 2>/dev/null')
+    print 'Clearing tc rules on server "'+str(serverIp)+'" over.'
+
+
+    mySsh.closeSsh()
+
+
+def initNetworkConfig(serverIp, username='root', passwd='fortinet', serverPort=22):
+    mySsh = SshRemote.SshAgent(serverIp, username, passwd, serverPort)
+    mySsh.connectRemote()
+
+    mySsh.execCommand('sysctl -w net.ipv4.ip_forward=1')
+    mySsh.execCommand('sysctl -w net.ipv6.conf.all.forwarding=1')
+
+    intList = mySsh.execCommand('ip link|grep -E -v "^ "|awk \'{print $2}\'|awk -F ":" \'{print $1}\'')
+    for i in intList[0]:
+        mySsh.execCommand('ip link set '+str(i).replace('\n', '')+' up')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print ''
+        print 'Usage:'
+        print './ClearServerNetConfig.py <Server IP>'
+        print ''
+        exit()
+
+    clearNetworkConfig(sys.argv[1])
+    initNetworkConfig(sys.argv[1])
+
+
+
+
+
+
+
---
+++ SshRemote.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+import paramiko
+
+
+class SshAgent():
+    def __init__(self, remoteIp, username, passwd, remotePort=22):
+        self.remoteIp = remoteIp
+        self.username = username
+        self.passwd = passwd
+        self.remotePort = remotePort
+
+        self.ssh = paramiko.SSHClient()
+        self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+
+    def connectRemote(self):
+        print 'Connecting remote server "'+self.remoteIp+':'+str(self.remotePort)+'" ...'
+        self.ssh.connect(self.remoteIp, self.remotePort, self.username, self.passwd)
+        print 'Connecting remote server "'+self.remoteIp+':'+str(self.remotePort)+' succeeds.'
+
+    def execCommand(self, command):
+        print 'Executing command "'+command+'" on server "'+self.remoteIp+':'+str(self.remotePort)+'" ...'
+        stdin, stdout, stderr = self.ssh.exec_command(command)
+        stdoutList = stdout.readlines()
+        stderrList = stderr.readlines()
+        #print 'The result from remote server "'+self.remoteIp+':'+str(self.remotePort)+'" is:'
+        if stdoutList != []:
+            print ''
+            for i in stdoutList:
+                print i,
+            print ''
+        if stderrList != []:
+            print 'The error from remote server "'+self.remoteIp+':'+str(self.remotePort)+'" is:\n'
+            for j in stderrList:
+                print j,
+            print ''
+        return stdoutList, stderrList
+
+
+    def closeSsh(self):
+        print 'Closing the connection to server "'+self.remoteIp+':'+str(self.remotePort)+'" ...'
+        self.ssh.close()
+        print 'Connection to server "'+self.remoteIp+':'+str(self.remotePort)+'" has been closed.'
+
+
+
+
